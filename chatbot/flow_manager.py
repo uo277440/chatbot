fe387cbe140715b524f4bd7433ee37853077f92f
uo277440 <uo277440@uniovi.es>
2024-03-26 17:55:25 +0100

    flujos de conversación

---
+++ chatbot/flow_manager.py
@@ -0,0 +1,41 @@
+import json
+
+class FlowManager:
+    def __init__(self, json_file, flow_name):
+        self.flow = self.load_flow(json_file, flow_name) 
+        self.next_options = ["GREETING"]
+        self.finished = False
+
+    def load_flow(self, json_file, flow_name):
+        with open(json_file, 'r') as file:
+            flows_data = json.load(file)
+        flows = flows_data.get('flows', [])
+        for flow in flows:
+            if flow.get('name') == flow_name:
+                return flow
+        return None
+    
+    def advance(self, next_label):
+        if next_label in self.next_options:
+            for step in self.flow.get('steps', []):
+                if step['label'] == next_label:
+                    self.next_options = step.get('options', [])
+                    if not self.next_options:
+                        self.finished=True
+                        print("FLUJO TERMINADO")
+                    return True
+        return False
+
+# Uso del FlowManager
+flow_manager = FlowManager('hotel_flujos.json', 'RESERVATION_FLOW')
+print(flow_manager.advance("GREETING"))
+print(flow_manager.advance("CHECK_AVAILABILITYd"))
+print(flow_manager.next_options)
+print(flow_manager.advance("CHECK_AVAILABILITY"))
+print(flow_manager.next_options)
+print(flow_manager.advance("TAKE_DETAILS"))
+print(flow_manager.next_options)
+print(flow_manager.advance("CONFIRM_RESERVATION"))
+print(flow_manager.advance("ASK_QUESTION"))
+print(flow_manager.next_options)
+print(flow_manager.advance("FAREWELL"))
