8273e4e672937a64847a6cf020752c09e45e7303
uo277440 <uo277440@uniovi.es>
2024-05-01 14:48:35 +0200

    corregido error username

--- myapi/serializer.py
+++ myapi/serializer.py
@@ -9,8 +9,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 		model = UserModel
 		fields = '__all__'
 	def create(self, clean_data):
-		user_obj = UserModel.objects.create_user(email=clean_data['email'], password=clean_data['password'])
-		user_obj.username = clean_data['username']
+		user_obj = UserModel.objects.create_user(email=clean_data['email'],username=clean_data['username'],password=clean_data['password'])
 		user_obj.save()
 		return user_obj
 
