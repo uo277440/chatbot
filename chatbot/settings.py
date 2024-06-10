3967ebc8609d083ad3160d7c7233fd8085946864
uo277440 <uo277440@uniovi.es>
2024-06-10 22:37:18 +0200

    settings cambiado

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 import sys
 from decouple import config
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
