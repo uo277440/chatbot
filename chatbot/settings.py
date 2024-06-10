0083c138481025499a4474548f979f376b41d51d
uo277440 <uo277440@uniovi.es>
2024-06-10 22:13:58 +0200

    heroku redis

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import sys
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,10 +60,20 @@ MIDDLEWARE = [
      
 ]
 ASGI_APPLICATION = 'chatbot.asgi.application'
-
+'''
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
+'''
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [config('REDIS_URL')],
+        },
     },
 }
 CORS_ORIGIN_ALLOW_ALL = True
