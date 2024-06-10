b81a061f46b854e664a2b79ccc89eef9242e274d
uo277440 <uo277440@uniovi.es>
2024-06-10 23:01:10 +0200

    cambio wsgi despliegue

--- chatbot/wsgi.py
+++ chatbot/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
-
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
 application = get_wsgi_application()
