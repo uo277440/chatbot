43be0861959895d59969fc12c461dd62f41a9de8
uo277440 <uo277440@uniovi.es>
2024-06-12 01:43:05 +0200

    cambios deploy

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -100,7 +100,7 @@ ROOT_URLCONF = 'chatbot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'chatfront', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
