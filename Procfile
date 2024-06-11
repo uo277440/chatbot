0241fe62f2e7c8f380b5b370f5352c0ce30aae78
uo277440 <uo277440@uniovi.es>
2024-06-11 18:23:58 +0200

    cambios deploy

--- Procfile
+++ Procfile
@@ -1,5 +1,6 @@
 release: python manage.py migrate
-web: sh -c 'cd chatfront && npm install && npm run build && cd .. && python manage.py collectstatic --noinput && daphne -b 0.0.0.0 -p $PORT chatbot.asgi:application'
+web: node server.js
+daphne: daphne -b 0.0.0.0 -p $PORT chatbot.asgi:application
 
 
 
--- server.js
+++ server.js
@@ -3,23 +3,27 @@ const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-// Middleware para servir archivos estáticos
+
 app.use(express.static(path.join(__dirname, 'chatfront/build')));
 
 // Proxy requests to /api to Django
 app.use('/api', createProxyMiddleware({
-  target: 'http://localhost:8000',
+  target: 'http://localhost:8000', 
   changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', 
+  },
 }));
 
-// Servir el archivo index.html para cualquier ruta no definida
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'chatfront/build', 'index.html'));
 });
 
-// Configurar el puerto y escuchar
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
