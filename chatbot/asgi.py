3ba5c3dcfc7b0585ab54a561ad4e8f6e84f8f343
uo277440 <uo277440@uniovi.es>
2024-06-11 14:56:23 +0200

    mejora para el despliegue asgi

--- chatbot/asgi.py
+++ chatbot/asgi.py
@@ -1,13 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 import myapi.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # Manejador HTTP de Django
+    "http": django_asgi_app,  # Manejador HTTP de Django
     'websocket':AuthMiddlewareStack(
         URLRouter(
             myapi.routing.websocket_urlpatterns
