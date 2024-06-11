b355cc28d3c90648918fff4214d683dbf472a857
uo277440 <uo277440@uniovi.es>
2024-06-11 14:44:57 +0200

    cambio urls despliegue

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapi.urls'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
