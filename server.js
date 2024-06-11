f6b7d048b51519134e7de80acd59dbd4265feddf
uo277440 <uo277440@uniovi.es>
2024-06-11 17:38:01 +0200

    cambios despliegue

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'chatfront/build')));
+
+// Proxy requests to /api to Django
+app.use('/api', createProxyMiddleware({
+  target: 'http://localhost:8000',
+  changeOrigin: true,
+}));
 
 // Servir el archivo index.html para cualquier ruta no definida
 app.get('*', (req, res) => {
