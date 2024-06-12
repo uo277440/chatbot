fc584e19389a1d7ca9d1d5bc4f3c35dae6dbb4a1
uo277440 <uo277440@uniovi.es>
2024-06-12 21:00:08 +0200

    cambiop settings

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/frontend/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
