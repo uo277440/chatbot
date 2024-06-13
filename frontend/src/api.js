0dc5f8b389a2461367b8570d2316c226954cf5c4
uo277440 <uo277440@uniovi.es>
2024-06-13 14:19:22 +0200

    añadida url backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/chatbottfg/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
