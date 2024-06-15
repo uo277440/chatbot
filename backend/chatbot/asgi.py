01cb53de0e0ea5494ef39f468aa07c971684fa55
uo277440 <uo277440@uniovi.es>
2024-06-15 21:05:47 +0200

    prueba websockets

--- backend/chatbot/asgi.py
+++ backend/chatbot/asgi.py
@@ -8,15 +8,14 @@ django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 import myapi.routing
 from channels.auth import AuthMiddlewareStack
+from myapi.routing import websocket_urlpatterns
 
 
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,  # Manejador HTTP de Django
-    'websocket':AuthMiddlewareStack(
-        URLRouter(
-            myapi.routing.websocket_urlpatterns
-        )
-    )
+    "http": django_asgi_app,
+    "websocket": URLRouter(
+        websocket_urlpatterns
+    ),
 })
 
