93950187385bc42e030e3631cf0603869d564cdc
uo277440 <uo277440@uniovi.es>
2024-06-16 19:38:29 +0200

    cambio settings para horario español

--- backend/chatbot/settings.py
+++ backend/chatbot/settings.py
@@ -164,9 +164,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
