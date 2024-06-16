99390cb380c985614e6fb03c444098fd042f266c
uo277440 <uo277440@uniovi.es>
2024-06-16 17:52:25 +0200

    cambio despliegue

--- backend/myapi/models.py
+++ backend/myapi/models.py
@@ -11,8 +11,31 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-import firebase_config
+import os
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials
 
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener las credenciales desde las variables de entorno
+cred = credentials.Certificate({
+    "type": os.getenv("GOOGLE_TYPE"),
+    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+    "auth_uri": os.getenv("GOOGLE_AUTH_URI"),
+    "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL")
+})
+
+# Inicializar Firebase
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 class AppUserManager(BaseUserManager):
 	def create_user(self,email,username, password=None):
 		if not email:
@@ -139,7 +162,6 @@ class ForumMessage(models.Model):
         super().delete(*args, **kwargs)
 
     def update_firestore(self, action):
-        db = firestore.client()
         message_data = {
             'id': self.id,
             'message': self.message,
