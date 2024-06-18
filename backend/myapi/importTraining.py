3dc368ff12e4b24559054d391eb5472c8f45db7d
uo277440 <uo277440@uniovi.es>
2024-06-18 19:45:45 +0200

    mas mejoras de rendimiento

--- backend/myapi/importTraining.py
+++ backend/myapi/importTraining.py
@@ -3,12 +3,21 @@ import io
 from django.core.exceptions import ObjectDoesNotExist
 from .models import ChatbotData, Flow
 
-def cargar_datos_csv_a_bd(csv_reader, flow):
-    print(csv_reader)
-    print("CARGAR DATOS A BD")
-    for row in csv_reader:
+def cargar_datos_csv_a_bd(csv_data, flow, batch_size=500):
+    chat_data_instances = []
+
+    for row in csv_data:
         user_input = row['User Input']
         label = row['Label']
-        ChatbotData.objects.create(user_input=user_input, label=label, flow=flow)
+        chat_data_instances.append(ChatbotData(user_input=user_input, label=label, flow=flow))
+
+        # Insert batch_size number of records at a time
+        if len(chat_data_instances) >= batch_size:
+            ChatbotData.objects.bulk_create(chat_data_instances)
+            chat_data_instances = []
+
+    # Insert any remaining records
+    if chat_data_instances:
+        ChatbotData.objects.bulk_create(chat_data_instances)
     
 
