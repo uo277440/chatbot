defeb1c0a187e3471879393b7b364efcc2c0d405
uo277440 <uo277440@uniovi.es>
2024-07-08 23:15:44 +0200

    cambio inicio SVM

--- backend/myapi/views.py
+++ backend/myapi/views.py
@@ -103,6 +103,13 @@ def update_flow_manager(request):
         first_charge = True
     else:
         if(flow.id == flowManager.id):
+            model_path = f'models/svm_model_{flow_id}.pkl'
+            os.makedirs('models', exist_ok=True)
+            new_chatbot = SVMChatbot(generar_csv_entrenamiento(flow.id), model_path=model_path)
+            if not new_chatbot.load_model():
+                new_chatbot.load_data()  
+                new_chatbot.train_model()
+            set_session_objects(request.session, new_chatbot, flowManager, marker)
             return JsonResponse({'message': 'flowManager actualizado correctamente','first_charge':first_charge},status=status.HTTP_200_OK)
     # Definir la ruta del archivo del modelo específico para el flujo
     model_path = f'models/svm_model_{flow_id}.pkl'
